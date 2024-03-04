fc1c1f1c28a8b3379cd4f5d8b890203b1c46696c
Anas07git <anas.karatela@gmail.com>
2024-03-05 01:27:32 +0530

    tailwindcss

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const Title=()=>(
     <a href="/">
-            <img alt="logo" className="logo" src="https://lh3.googleusercontent.com/Em7AHf7XBH_RtGfCBVXz9RH8SM_pHkj3xPP-yd3cRguY1_Jc8fmqgx6WxnvGVyPV5xs5gL3HCD0FCuv6Xo4CwoY6ak4"/>
+            <img alt="logo" className="h-28 p-2" src="https://lh3.googleusercontent.com/Em7AHf7XBH_RtGfCBVXz9RH8SM_pHkj3xPP-yd3cRguY1_Jc8fmqgx6WxnvGVyPV5xs5gL3HCD0FCuv6Xo4CwoY6ak4"/>
 
     </a>
 )
@@ -14,15 +14,15 @@ const Title=()=>(
 const Header=()=>{
     const[isLoggedIn,setIsLoggedIn]=useState(false)
     return (
-        <div className="header">
+    <div className="flex justify-between bg-orange-100 shadow-lg">
          <Title/>
             <div className="navList">
-                <ul>
+                <ul className="flex py-10">
                     
-                    <li> <Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact </Link></li>
-                    <li><Link to="/cart">Cart </Link></li>
+                    <li className="px-2"> <Link to="/">Home</Link></li>
+                    <li className="px-2" ><Link to="/about">About</Link></li>
+                    <li className="px-2"><Link to="/contact">Contact </Link></li>
+                    <li className="px-2"><Link to="/cart">Cart </Link></li>
                 </ul>
             </div>
          <button onClick={()=>setIsLoggedIn(!isLoggedIn)}>{isLoggedIn?"LogOut":"LogIn"}</button>
